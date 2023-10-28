b55e6f5a5dc835d3b40a10aca09bb8421d6f621e
Ant <anthony.hillairet@gmail.com>
2023-10-28 12:27:31 -0700

    Handle Aged Brie

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -16,7 +16,15 @@ def _update_item(item):
     if item_ext.is_sulfuras:
         return
 
-    if (not item_ext.is_aged_brie) and (not item_ext.is_backstage_passes):
+    if item_ext.is_aged_brie:
+        quality_increment = 1 if item.sell_in > 0 else 2
+        item.sell_in = item.sell_in - 1
+        if item.quality > 49:
+            return
+        item.quality = item.quality + quality_increment
+        return
+
+    if not item_ext.is_backstage_passes:
         if item.quality > 0:
             item.quality = item.quality - 1
     else:
@@ -31,15 +39,11 @@ def _update_item(item):
                         item.quality = item.quality + 1
     item.sell_in = item.sell_in - 1
     if item.sell_in < 0:
-        if not item_ext.is_aged_brie:
-            if not item_ext.is_backstage_passes:
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-            else:
-                item.quality = item.quality - item.quality
+        if not item_ext.is_backstage_passes:
+            if item.quality > 0:
+                item.quality = item.quality - 1
         else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
+            item.quality = item.quality - item.quality
 
 
 class Item:
