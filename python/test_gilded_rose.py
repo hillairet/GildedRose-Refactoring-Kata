29c96628c28afa9160838710c53178124ff260b9
Ant <anthony.hillairet@gmail.com>
2023-10-28 05:56:55 -0700

    Switch to pytest and make basic test

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
-import unittest
+from pytest import mark, param
 
 from gilded_rose import GildedRose, Item
 
-
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+item_fixture = [param("+5 Dexterity Vest", 10, 20)]
 
 
-if __name__ == '__main__':
-    unittest.main()
+@mark.parametrize('name, sell_in, quality', item_fixture)
+def test_gilded_rose(name, sell_in, quality):
+    items = [Item(name, sell_in, quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].sell_in == sell_in - 1
