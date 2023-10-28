51ebdf5f37f52848b4cbe7d11a132d20a61584fd
Ant <anthony.hillairet@gmail.com>
2023-10-28 13:27:14 -0700

    Handle standard items

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -27,13 +27,7 @@ def _update_item(item):
         update_conjured(item)
         return
 
-    if item.quality > 0:
-        item.quality = item.quality - 1
-
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0:
-        if item.quality > 0:
-            item.quality = item.quality - 1
+    _update_standard_item(item)
 
 
 class Item:
@@ -101,3 +95,15 @@ def _check_for_conjured(item: Item) -> Optional[Callable]:
         item.quality = item.quality - 2
 
     return _update_conjured
+
+
+def _update_standard_item(item: Item) -> None:
+    item.sell_in = item.sell_in - 1
+
+    increment = 2 if item.sell_in < 0 else 1
+
+    if item.quality < increment:
+        item.quality = 0
+        return
+
+    item.quality = item.quality - increment
