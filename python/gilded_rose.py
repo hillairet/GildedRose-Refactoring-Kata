374142e4e7112f49afb16a56f8520f6082ba44af
Ant <anthony.hillairet@gmail.com>
2023-10-28 07:46:40 -0700

    Exit early for Sulfuras

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -12,10 +12,13 @@ class GildedRose(object):
 
 def _update_item(item):
     item_ext = ItemExtended(name=item.name, sell_in=item.sell_in, quality=item.quality)
+
+    if item_ext.is_sulfuras:
+        return
+
     if (not item_ext.is_aged_brie) and (not item_ext.is_backstage_passes):
         if item.quality > 0:
-            if not item_ext.is_sulfuras:
-                item.quality = item.quality - 1
+            item.quality = item.quality - 1
     else:
         if item.quality < 50:
             item.quality = item.quality + 1
@@ -26,14 +29,12 @@ def _update_item(item):
                 if item.sell_in < 6:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-    if not item_ext.is_sulfuras:
-        item.sell_in = item.sell_in - 1
+    item.sell_in = item.sell_in - 1
     if item.sell_in < 0:
         if not item_ext.is_aged_brie:
             if not item_ext.is_backstage_passes:
                 if item.quality > 0:
-                    if not item_ext.is_sulfuras:
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - 1
             else:
                 item.quality = item.quality - item.quality
         else:
